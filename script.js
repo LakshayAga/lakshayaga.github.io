68dc45764b2a0dfcaaec7c68374ab380276c1d62
LakshayAga <lakshay.agrwl@gmail.com>
2026-05-04 23:56:24 +0530

    init

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('year').textContent = new Date().getFullYear();
+
+// Future: mount three.js scene into #hero-canvas
+// import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
+// const mount = document.getElementById('hero-canvas');
+// ... init renderer, camera, scene, append renderer.domElement to mount
